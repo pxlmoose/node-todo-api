1f7004aa8b3adffdaa5185ace488d698157de634
Jan Nowicki <pxlmoose@gmail.com>
2018-06-01 13:29:44 +0200

    Test DELETE /todos:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -100,7 +100,43 @@ describe('GET /todos/:id', () => {
     .expect(404)
     .end(done);
   });
+});
 
+describe('DELETE /todos:id', () => {
+  it('should remove a todo', (done) => {
+    let hexID = todos[1]._id.toHexString();
 
+    request(app)
+    .delete(`/todos/${hexID}`)
+    .expect(200)
+    .expect((res) => {
+      expect(res.body.todo._id).toBe(hexID);
+    })
+    .end((err, res) => {
+      if(err) {
+        return done(err);
+      }
 
+      Todo.findById(hexID).then((todo) => {
+        expect(todo).toNotExist();
+        done();
+      }).catch((e) => done(e));
+    });
+  });
+
+  it('should return 404 if todo was not found', (done) => {
+    let hexID = new ObjectID().toHexString();
+
+    request(app)
+    .delete(`/todos/${hexID}`)
+    .expect(404)
+    .end(done);
+  });
+
+  it('should return 404 for wrong id', (done) => {
+    request(app)
+    .delete('/todos/123')
+    .expect(404)
+    .end(done);
+  });
 });
