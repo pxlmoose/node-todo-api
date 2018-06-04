b780f5da14869f7f334e767a838b8c4b96509145
Jan Nowicki <pxlmoose@gmail.com>
2018-06-04 21:20:16 +0200

    Update config to user json file

--- server/config/config.js
+++ server/config/config.js
@@ -1,10 +1,10 @@
 const env = process.env.NODE_ENV || 'development';
 
+if(env === 'development' || env === 'test') {
+  let config = require('./config.json');
+  let envConfig = config[env];  // you use brackets when using variable to access property
 
-if (env === 'development') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI =  'mongodb://localhost:27017/TodoApp';
-} else if (env === 'test') {
-  process.env.PORT = 3000;
-  process.env.MONGODB_URI =  'mongodb://localhost:27017/TodoAppTest';
+  Object.keys(envConfig).forEach((key) => {
+    process.env[key] = envConfig[key];
+  });
 }
